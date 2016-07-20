d2335083a435d0c658c5b25a6d88a092e0a12bf6
Tydon K <tydonk@gmail.com>
2016-07-20 16:19:13 +0000

    added relationships to models. added comments/outline

--- tbay.py
+++ tbay.py
@@ -8,19 +8,16 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-user_auction_items_table = Table('user_auction_items_association', Base.metadata,
-    Column('user_id', Integer, ForeignKey('user.id')),
-    Column('item_id', Integer, ForeignKey('item.id'))
-)
-    
 class User(Base):
     __tablename__ = "user"
     
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    auction_items = relationship("Item", secondary="user_auction_items_association", backref="users")
-
+    
+    item = relationship("Item", backref="owner")
+    bid = relationship("Bid", backref="bidder")
+    
 class Item(Base):
     __tablename__ = "item"
     
@@ -29,11 +26,18 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     
+    bids = relationship("Bid", backref="bid")
+    
+    owner_id = Column(Integer, ForeignKey('owner.id'), nullable=False)
+    
 class Bid(Base):
     __tablename__ = "bid"
     
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
+    
+    bidder_id = Column(Integer, ForeignKey('bidder.id'), nullable=False)
+    bid_id = Column(Integer, ForeignKey('bid.id'), nullable=False)
     
 Base.metadata.create_all(engine)
 
@@ -42,15 +46,23 @@ Base.metadata.create_all(engine)
 # Multiple users should be able to place a bid on an item.
 
 #RELATIONSHIPS:
-    # User -> Item = MANY to ONE
-    # User -> Bid = MANY to ONE
-    # Item -> Bid = MANY to MANY
+    # User -> Item = ONE to MANY
+    # User -> Bid = ONE to MANY
+    # Item -> Bid = ONE to MANY
     
 # Add three users to the database:
+edward_nigma = User(username="Riddler", password="riddle_me_this")
+victor_fries = User(username="MrFreeze", password="everybody_chill")
+pamela_isley = User(username="PoisonIvy", password="so_little_time")
 
 # Make one user auction a baseball:
+    # 
+baseball = Item(name="baseball", description="A real baseball", owner=victor_fries)
 
 # Have each other user place two bids on the baseball:
 
+
 # Perform a query to find out which user placed the highest bid:
+
+
     
